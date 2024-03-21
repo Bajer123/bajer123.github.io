b39fba0ed7c7e3f8e6fe7340f3d10cab7006eae6
au661858 <au661858>
2024-03-21 11:52:59 +0100

    Added my own model

--- transferlearning/my_classes.js
+++ transferlearning/my_classes.js
@@ -1,10 +1,5 @@
 export const IMAGENET_CLASSES = {
-  0: 'bird',
-  1: 'cat',
-  2: 'cows',
-  3: 'dog',
-  4: 'horse',
-  5: 'mouse',
-  6: 'pig',
-  7: 'sheep'
+  0: 'RedWine',
+  1: 'WhiteWine',
+  2: 'Rose',
 };
