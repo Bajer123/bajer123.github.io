436c2af497603c4ea0df5b239db967bce851fdc9
Asta <201806556@post.au.dk>
2024-03-21 12:19:39 +0100

    goodbye food image

--- transferlearning/index.js
+++ transferlearning/index.js
@@ -63,12 +63,12 @@ const mobilenetDemo = async () => {
   // Warmup the model. This isn't necessary, but makes the first prediction
   // faster. Call `dispose` to release the WebGL memory allocated for the return
   // value of `predict`.
-  mobilenet.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
+  //mobilenet.predict(tf.zeros([1, IMAGE_SIZE, IMAGE_SIZE, 3])).dispose();
 
   status('');
 
   // Make a prediction through the locally hosted cat.jpg.
-  const catElement = document.getElementById('cat');
+  /**const catElement = document.getElementById('cat');
   if (catElement.complete && catElement.naturalHeight !== 0) {
     predict(catElement);
     catElement.style.display = '';
@@ -78,6 +78,7 @@ const mobilenetDemo = async () => {
       catElement.style.display = '';
     }
   }
+  */
 
   document.getElementById('file-container').style.display = '';
 };
